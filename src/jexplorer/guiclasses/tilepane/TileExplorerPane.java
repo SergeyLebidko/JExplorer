190407722d587d550e409465f50b1200ab5c6c25
SergeyLebidko <it.sergeyler@mail.ru>
2019-03-20 15:23:35 +0300

    Проведен рефакторинг класса TileExplorerPane

--- src/jexplorer/guiclasses/tilepane/TileExplorerPane.java
+++ src/jexplorer/guiclasses/tilepane/TileExplorerPane.java
@@ -18,7 +18,10 @@ public class TileExplorerPane implements ExplorerPane {
     private AdaptiveGridLayout currentLayout;
     private FileSystemExplorer fileSystemExplorer;
 
-    private final Color backColor=Color.WHITE;
+    private final Color backColor = Color.WHITE;
+    private final Color textColorForNoHidden = Color.BLACK;
+    private final Color textColorForHidden = new Color(130,130,130);
+
 
     class AdaptiveGridLayout implements LayoutManager {
 
@@ -53,12 +56,12 @@ public class TileExplorerPane implements ExplorerPane {
             }
         }
 
-        public boolean isBigCells(){
-            return currentSizeCell==BIG_CELLS;
+        public boolean isBigCells() {
+            return currentSizeCell == BIG_CELLS;
         }
 
-        public boolean isSmallCells(){
-            return currentSizeCell==SMALL_CELLS;
+        public boolean isSmallCells() {
+            return currentSizeCell == SMALL_CELLS;
         }
 
         @Override
@@ -116,14 +119,14 @@ public class TileExplorerPane implements ExplorerPane {
     }
 
     public TileExplorerPane() {
-        fileSystemExplorer= MainClass.getFileSystemExplorer();
+        fileSystemExplorer = MainClass.getFileSystemExplorer();
 
         contentPane = new JPanel();
         currentLayout = new AdaptiveGridLayout(AdaptiveGridLayout.BIG_CELLS);
         contentPane.setLayout(currentLayout);
         contentPane.setBackground(backColor);
 
-        scrollPane=new JScrollPane(contentPane);
+        scrollPane = new JScrollPane(contentPane);
         scrollPane.getVerticalScrollBar().setUnitIncrement(20);
         scrollPane.addComponentListener(new ComponentAdapter() {
             @Override
@@ -144,28 +147,28 @@ public class TileExplorerPane implements ExplorerPane {
         clearContentPane();
 
         //Добавляем на панель контента новые элементы
-        File[] elements=fileSystemExplorer.getCurrentElementsList();
+        File[] elements = fileSystemExplorer.getCurrentElementsList();
         content = new JLabel[elements.length];
         int i = 0;
+
+        //Вначале добавляем каталоги
         for (File element : elements) {
+            if (!element.isDirectory()) continue;
             content[i] = new JLabel();
-            content[i].setText(element.getName());
-            content[i].setToolTipText(element.getName());
-
-            if (currentLayout.isBigCells()){
-                content[i].setIcon(new ImageIcon("res\\tileView\\folder_big.png"));
-            }
-            if (currentLayout.isSmallCells()){
-                content[i].setIcon(new ImageIcon("res\\tileView\\folder_small.png"));
-            }
-
-            content[i].setHorizontalTextPosition(SwingConstants.CENTER);
-            content[i].setVerticalTextPosition(SwingConstants.BOTTOM);
-            content[i].setHorizontalAlignment(SwingConstants.CENTER);
-            content[i].setBackground(backColor);
+            setParameters(element, content[i]);
             contentPane.add(content[i]);
             i++;
         }
+
+        //Затем добавляем файлы
+        for (File element : elements) {
+            if (!element.isFile()) continue;
+            content[i] = new JLabel();
+            setParameters(element, content[i]);
+            contentPane.add(content[i]);
+            i++;
+        }
+
         scrollPane.repaint();
     }
 
@@ -175,6 +178,59 @@ public class TileExplorerPane implements ExplorerPane {
         for (Component component : components) {
             contentPane.remove(component);
         }
+    }
+
+    private void setParameters(File element, JLabel lab){
+        setText(element, lab);
+        setIcon(element, lab);
+        setTextColors(element, lab);
+        setBorders(lab);
+        setAligments(lab);
+    }
+
+    private void setText(File element, JLabel lab){
+        lab.setText(element.getName());
+        lab.setToolTipText(element.getName());
+    }
+
+    private void setIcon(File element, JLabel lab) {
+        if (element.isDirectory()) {
+            if (currentLayout.isBigCells()) {
+                if (element.isHidden()) lab.setIcon(new ImageIcon("res\\tileView\\folder_big_hidden.png"));
+                if (!element.isHidden()) lab.setIcon(new ImageIcon("res\\tileView\\folder_big.png"));
+            }
+            if (currentLayout.isSmallCells()) {
+                if (element.isHidden()) lab.setIcon(new ImageIcon("res\\tileView\\folder_small_hidden.png"));
+                if (!element.isHidden()) lab.setIcon(new ImageIcon("res\\tileView\\folder_small.png"));
+            }
+        }
+        if (element.isFile()){
+            if (currentLayout.isBigCells()) {
+                //Вставить код выбора подходящего значка
+            }
+            if (currentLayout.isSmallCells()) {
+                //вставить код выбора подходящего значка
+            }
+        }
+    }
+
+    private void setTextColors(File element, JLabel lab){
+        if (element.isHidden()) {
+            lab.setForeground(textColorForHidden);
+        }
+        if (!element.isHidden()) {
+            lab.setForeground(textColorForNoHidden);
+        }
+    }
+
+    private void setBorders(JLabel lab) {
+        lab.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
+    }
+
+    private void setAligments(JLabel lab) {
+        lab.setHorizontalTextPosition(SwingConstants.CENTER);
+        lab.setVerticalTextPosition(SwingConstants.BOTTOM);
+        lab.setHorizontalAlignment(SwingConstants.CENTER);
     }
 
 }
